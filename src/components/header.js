76030dc170f836ae5fc71ed17d41372144aaa164
zh_New <739493331@qq.com>
2020-10-30 17:46:42 +0800

    CSS

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import './header.css';
 
 class home extends Component {
     render() {
         return (
             <div>
                 我是页头组件
+                <div style={{width:'100px',height:'100px'}} className="div1"></div>
             </div>
         );
     }
--- src/components/hookzujian.js
+++ src/components/hookzujian.js
@@ -1,10 +1,12 @@
 import { useState } from 'react';
+import './hookzujian.css';
 
 export default function Hkzj(){
     let [val, setVal] = useState(0);
     return (
         <div>
             <hr/>
+            <div style={{width:'100px',height:'100px'}} className="div1"></div>
             <span>我使用hook 让无状态组件 可以使用状态</span>
             <p>这是状态数据: {val}</p>
             <button onClick={()=>{ setVal(val+1) }}>add状态数据</button>
